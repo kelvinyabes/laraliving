4ed48e1a853cb1ccead718a30ac42baec58fb827
prayogo19 <jannyeka98@gmail.com>
2020-07-09 16:03:57 +0700

    add relationship

--- app/Pertanyaan.php
+++ app/Pertanyaan.php
@@ -9,6 +9,19 @@ class Pertanyaan extends Model
     //
     protected $table = "pertanyaan";
 
+
+    // reationship many-to-one ke table user
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+    
+    //relationship one-to-many ke table jawaban
+    public function jawaban()
+    {
+        return $this->hasMany('App\Models\JawabanModel', 'pertanyaan_id');
+    }
+
     public function likes(){
         return $this->belongsToMany('App\Like', 'like_tag');
     }
