e036db9bab4a8c4e12f6daf2fdcab576c2f116cd
prayogo19 <jannyeka98@gmail.com>
2020-07-09 16:02:46 +0700

    add relationship

--- app/Models/JawabanModel.php
+++ app/Models/JawabanModel.php
@@ -20,4 +20,16 @@ class JawabanModel{
         $jawaban = DB::table('jawaban')->where('pertanyaan_id', $pertanyaan_id)->get();
         return $jawaban;
     }
+
+    // relationship many-to-one ke tabel user
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    // relationship many-to-one ke tabel pertanyaan 
+    public function pertanyaan()
+    {
+        return $this->belongsTo('App\Models\PertanyaanModels', 'pertanyaan_id');
+    }
 }
