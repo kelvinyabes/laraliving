1fe98c4c661f1d2483576f5225f4249424a52670
prayogo19 <jannyeka98@gmail.com>
2020-07-12 19:26:48 +0700

    fixing commit

--- config/database.php
+++ config/database.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url('postgres://pxpgqaubidmywz:13a399bee5fdf6ff6e123e2284915df2c02ca7da315d4d2f70f3443167672b93@ec2-52-200-48-116.compute-1.amazonaws.com:5432/deb52khtqbn9vp');
-
 return [
 
     /*
@@ -17,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => ('DATABASE_URL'),
-            'host' => $DATABASE_URL("host"),
-            'port' => $DATABASE_URL("port"),
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL("user"),
-            'password' => $DATABASE_URL("pass"),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
